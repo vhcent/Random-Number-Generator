50d3d9716cd060497b7b6f3395a2a807bbd9d5c2
Aroop Biswal <aroopbiswal@gmail.com>
2022-01-01 20:08:49 -0800

    minor changes

--- popup.js
+++ popup.js
@@ -1,6 +1,21 @@
 document.addEventListener('DOMContentLoaded', function () {
     var checkButton = document.getElementById('check');
+
     checkButton.addEventListener('click', function () {
-        alert("Hey your button is working!");
+        //alert("Hey your button is working!");
+        var elements = document.getElementById("form").elements;
+        var objects = {};
+        for (var i = 0; i < elements.length; i++) {
+            var item = elements.item(i);
+            objects[item.name] = item.value;
+        }
+        var lowBound = objects['lowerBound'];
+        var upBound = objects['upperBound'];
+        var randomValue = Math.floor(Math.random() * (upBound - lowBound + 1) + upBound);
+        alert("randomValue = " + randomValue);
     }, false);
 }, false);
+
+function copy() {
+    let textarea = document.getElementById("textarea");
+}
