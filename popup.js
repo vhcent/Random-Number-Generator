e53ffd0d56514140402502bdc070af372e713c38
Aroop Biswal <aroopbiswal@gmail.com>
2022-01-01 19:35:37 -0800

    minor changes

---
+++ popup.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var checkButton = document.getElementById('check');
+    checkButton.addEventListener('click', function () {
+        alert("Hey your button is working!");
+    }, false);
+}, false);
