8507876b370211dba07a4fd6ec6f95f0d12c5b1c
Vincent Huang <84062739+vhcent@users.noreply.github.com>
2022-01-01 21:03:45 -0800

    Add files via upload

--- popup.js
+++ popup.js
@@ -1,18 +1,61 @@
 document.addEventListener('DOMContentLoaded', function () {
     var checkButton = document.getElementById('check');
+    var setBoundsButton = document.getElementById('setBounds');
+    var numbersGenerated = [];
+    var numbers = [];
+    for (var i = 0; i <= 100; i++) {
+        numbers.push(i);
+    }
 
-    checkButton.addEventListener('click', function () {
-        //alert("Hey your button is working!");
+    setBoundsButton.addEventListener('click', function () {
+        numbers = new Array();
         var elements = document.getElementById("form").elements;
         var objects = {};
         for (var i = 0; i < elements.length; i++) {
             var item = elements.item(i);
             objects[item.name] = item.value;
         }
-        var lowBound = objects['lowerBound'];
-        var upBound = objects['upperBound'];
-        var randomValue = Math.floor(Math.random() * (upBound - lowBound + 1) + upBound);
-        alert("randomValue = " + randomValue);
+        var lowBound = Number(objects['lowerBound']);
+        var upBound = Number(objects['upperBound']);
+        for (var i = lowBound; i <= upBound; i++) {
+            numbers.push(i);
+        }
+        /*
+        int index = 0;
+        for(i of range(lowBound, upBound))
+        {
+            numbers[index] = i;
+            index++;
+        }
+        */
+        //alert(numbers[0]);
+
+    }, false);
+    
+    checkButton.addEventListener('click', function () 
+    {
+        var elements = document.getElementById("form").elements;
+        var objects = {};
+        for (var i = 0; i < elements.length; i++) {
+            var item = elements.item(i);
+            objects[item.name] = item.value;
+        }
+        
+        var randomValue = numbers[Math.floor(Math.random() * numbers.length)];
+
+        //alert(numbers);
+        numbersGenerated.push(randomValue);
+        //numbers = ArrayUtils.removeElement(numbers, randomValue);
+        for(var i = 0; i < numbers.length; i++) 
+        {
+                if(numbers[i] == randomValue) {
+                    numbers.splice(i,1);
+                    break;
+                } 
+        }
+        alert(randomValue);
+        
+        //alert("randomValue = " + randomValue);
     }, false);
 }, false);
 
